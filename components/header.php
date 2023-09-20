d176a7b691bdcf3318d632e610789205b0fb76e2
br3-mah <nyeleti.bremah@gmail.com>
2023-09-20 17:59:38 +0200

    website updated

--- components/header.php
+++ components/header.php
@@ -25,16 +25,16 @@
                                                             <a href="services.php"><span>Our Services</span></a>
                                                             <ul class="sub-menu">
                                                                 <li id="menu-item-18280" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-18280">
-                                                                    <a href="#"><span>The Call Center</span></a>
+                                                                    <a onclick="redirectToService()" href="#"><span>The Call Center</span></a>
                                                                 </li>
                                                                 <li id="menu-item-18286" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-18286">
-                                                                    <a href="#"><span>Online Advisory Services</span></a>
+                                                                    <a onclick="redirectToService()" href="#"><span>Online Advisory Services</span></a>
                                                                 </li>
                                                                 <li id="menu-item-18288" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-18288">
-                                                                    <a href="marketplace.php"><span>The Marketplace</span></a>
+                                                                    <a onclick="redirectToService()" href="marketplace.php"><span>The Marketplace</span></a>
                                                                 </li>
                                                                 <li id="menu-item-18287" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-18287">
-                                                                    <a href="#"><span>Ecopay Payment Gateway</span></a>
+                                                                    <a onclick="redirectToService()" href="#"><span>Ecopay Payment Gateway</span></a>
                                                                 </li>
 
                                                             </ul>
@@ -103,23 +103,57 @@
                                     </div>
                                 </div>
                             </div>
+
                             <div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-3e4e340 sc_layouts_column_align_right sc_layouts_column sc_inner_width_none sc_content_align_inherit sc_layouts_column_icons_position_left sc_fly_static" data-id="3e4e340" data-element_type="column">
                                 <div class="elementor-column-wrap elementor-element-populated">
                                     <div class="elementor-widget-wrap">
-                                        <div class="sc_layouts_item elementor-element elementor-element-066af30 sc_fly_static elementor-widget elementor-widget-trx_sc_layouts_iconed_text" data-id="066af30" data-element_type="widget" data-widget_type="trx_sc_layouts_iconed_text.default">
+                                        
+                                        <!-- <div class="sc_layouts_item elementor-element elementor-element-066af30 sc_fly_static elementor-widget elementor-widget-trx_sc_layouts_iconed_text" data-id="066af30" data-element_type="widget" data-widget_type="trx_sc_layouts_iconed_text.default">
                                             <div class="elementor-widget-container">
-                                                <div class="sc_layouts_iconed_text"><a href="tel:18004585697" class="sc_layouts_item_link sc_layouts_iconed_text_link"><span class="sc_layouts_item_details sc_layouts_iconed_text_details"><span class="sc_layouts_item_details_line1 sc_layouts_iconed_text_line1">
-                                                                +26 097 458 56 97</span></span></a></div>
+                                                <div class="sc_layouts_iconed_text">
+                                                    <a href="tel:18004585697" class="sc_layouts_item_link sc_layouts_iconed_text_link">
+                                                        <span class="sc_layouts_item_details sc_layouts_iconed_text_details">
+                                                            <span class="sc_layouts_item_details_line1 sc_layouts_iconed_text_line1">+26 097 458 56 97</span>
+                                                        </span>
+                                                    </a>
+                                                </div>
                                             </div>
-                                        </div>
+                                        </div> -->
+                                        
                                         <div class="sc_layouts_item elementor-element elementor-element-d662b41 sc_fly_static elementor-widget elementor-widget-trx_sc_button" data-id="d662b41" data-element_type="widget" data-widget_type="trx_sc_button.default">
                                             <div class="elementor-widget-container">
-                                                <div class="sc_item_button sc_button_wrap"><a href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="sc_button sc_button_default sc_button_size_small sc_button_icon_left color_style_link2"><span class="sc_button_text"><span class="sc_button_title">Get Started</span></span></a></div>
+                                                <div class="sc_item_button sc_button_wrap">
+                                                    <a href="#" id="openSideMenu" data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="sc_button sc_button_default sc_button_size_small sc_button_icon_left color_style_link2">
+                                                        <span class="sc_button_text">
+                                                            <span class="sc_button_title">
+                                                                <!-- Check if user data is available in sessionStorage -->
+                                                                <!-- If yes, display the user's name -->
+                                                                <!-- If not, display "Get Started" -->
+                                                                <script>
+                                                                    const userData = JSON.parse(sessionStorage.getItem('user'));
+                                                                    if (userData && userData.name) {
+                                                                        document.write(userData.name);
+                                                                    } else {
+                                                                        document.write("Get Started");
+                                                                    }
+                                                                </script>
+                                                            </span>
+                                                        </span>
+                                                    </a>
+                                                    <a id="showLoginLink" style="display:none" href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="sc_button sc_button_default sc_button_size_small sc_button_icon_left color_style_link2">
+                                                        <span class="sc_button_text">
+                                                            <span class="sc_button_title">
+                                                                Login
+                                                            </span>
+                                                        </span>
+                                                    </a>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </section>
